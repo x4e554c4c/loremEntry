cf4cac5cb7c3cba7fd540412269f39b3b4e18b60
x4e554c4c <wanderingdwarf03@gmail.com>
2020-12-15 05:36:56 +0200

    Inputs animation preparation

--- src/services/inputs.js
+++ src/services/inputs.js
@@ -1,15 +1,82 @@
 class Inputs 
 {
-  targets = [];
+  _targets = [];
+
+  TAIL_LENGTH = 3;
+
+  ANIMATION_DELAY = 300;
 
   constructor(targets = []) {
-    this._targets = targets;
+    this.parse(targets);
   }
 
-  assign(generator) {
-    for (let input of this._targets) {
-      input.value = generator('text');
+  parse(inputs) {
+    for (let el of inputs) {
+      this._targets.push({
+        el, status: null,
+      });
     }
+  }
+
+  computeNewValues(generator) {
+    for (let input of this._targets) {
+      input.newValue = generator('text');
+    }
+  }
+
+  prepareForAnimation() {
+    let prepared = [...this._targets];
+
+    for (let i = 0; i < prepared.length; i++) {
+      let input = prepared[i];
+      
+      input.tail = 0;
+
+      // If the value is the same, removing it from the list.
+      if (input.el.value === input.newValue) {
+        input.status = null;
+        prepared.splice(i);
+        i--;
+
+        break
+      }
+
+      if (input.el.value) {
+        input.status = 'removing'; 
+        break;
+      }
+
+      input.status = 'appending';
+    } 
+
+    return prepared;
+  }
+  
+  animate(targets) {
+    for (let input of targets) {
+      input.el.value = Math.random();
+    }
+  }
+
+  clearAnimation() {
+    if (this.animation !== null)
+      clearInterval(this.animation);
+  }
+
+
+  assign(generator) {
+    // Clearing current animation (if it exists)
+    this.clearAnimation()
+
+    // Generating new value for each input
+    this.computeNewValues(generator);
+
+    // Computing animation status for targets
+    let animated = this.prepareForAnimation();
+
+    // Inializing animation loop
+    this.animation = 
+      setInterval(() => this.animate(animated), this.ANIMATION_DELAY);
   }
 }
 
